3a1c2d6ad55507c2c13d70e3dd2e0f6c18e5abd5
elad.bendavid <elad.bendavid1@cs.huji.ac.il>
2018-12-10 16:17:11 +0200

    rearranged path file

--- src/main/java/com/whistleBlower/demo/runnable/WhistleBlowerServer.java
+++ src/main/java/com/whistleBlower/demo/runnable/WhistleBlowerServer.java
@@ -1,14 +1,14 @@
-package com.whistleBlower.demo;
+package com.whistleBlower.demo.runnable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class DemoApplication {
+public class WhistleBlowerServer {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WhistleBlowerServer.class, args);
 	}
 }
